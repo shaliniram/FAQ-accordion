2fc705aa225261b828e9aa8c6817538f05b9cb4f
shaliniram <51158119+shaliniram@users.noreply.github.com>
2024-06-14 14:13:23 -0400

    updates toggle functionalities

--- index.js
+++ index.js
@@ -6,11 +6,15 @@ icons.forEach((icon)=>{
         const parent = icon.parentElement.nextElementSibling;
 
         console.log(parent.className)
-        parent.classList.remove('hide')
+        parent.classList.toggle('hide')
         const img = icon.querySelector('img'); // Find the img element within the div
         if (img) {
-           img.src = "assets/images/icon-minus.svg"; // Log the src attribute of the img element
-        } 
+            if (parent.classList.contains('hide')) {
+                img.src = "assets/images/icon-plus.svg"; // Image for hidden state
+            } else {
+                img.src = "assets/images/icon-minus.svg"; // Image for visible state
+            }
+        }
     })
 })
 
